1089e3f9f493a27fec4f4bf969b6e2b38ccbb384
juli82b4 <juliya.dian@gmail.com>
2026-02-15 21:54:35 +0100

    added backend and api end points to fetch cards from SQLite database

---
+++ backend/server.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('./database.db');
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS cards (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL
+    )
+`, (err) => {
+    //adding default cards if table is empty
+    db.get('SELECT COUNT(*) AS count FROM cards', (err, row) => {
+        if (err) {
+            console.error('Database error:', err);
+            return;
+        }
+
+        if (row.count === 0) {
+
+            const cards = [
+                "https://picsum.photos/id/13/200",
+                "https://picsum.photos/id/155/200",
+                "https://picsum.photos/id/193/200",
+                "https://picsum.photos/id/211/200",
+                "https://picsum.photos/id/237/200",
+                "https://picsum.photos/id/250/200"
+            ];
+
+            for (let i = 0; i < cards.length; i++) {
+                db.run(`INSERT INTO cards (name) VALUES ('${cards[i]}')`);
+            }
+
+            console.log("Default cards inserted!");
+        }
+    });
+});
+
+const app = express();
+
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello Express');
+});
+
+//Get all cards from SQLite
+app.get('/api/cards', (req, res) => {
+    db.all('SELECT * FROM cards', (err, rows) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Database error' });
+        }
+
+        res.json(rows);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
